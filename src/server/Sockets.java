06e703b3c94ec0f694ca202e070198c22e9dd177
Julian <julianvandenberg2002@gmail.com>
2020-04-23 19:55:41 +0200

    client werkt

---
+++ src/server/Sockets.java
@@ -0,0 +1,28 @@
+package server;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.UnknownHostException;
+import java.net.Socket;
+import java.util.Random;
+public class Sockets {
+    static Socket s;
+    static PrintWriter pw;
+
+    public static void main(String[] args) {
+        try {
+            s = new Socket("raspberrypi", 8000);
+            pw = new PrintWriter(s.getOutputStream());
+            pw.write("30");
+            pw.flush();
+            pw.close();
+            s.close();
+        }
+        catch (UnknownHostException e){
+            System.out.println("Fail");
+            e.printStackTrace();
+        }catch (IOException e){
+            System.out.println("Fail");
+            e.printStackTrace();
+        }
+    }
+}
